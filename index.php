7b13f486f0bb0d16e610a080e3c4d22d213ef6ff
Your Name <laura.kotlinska.96@gmail.com>
2025-10-05 16:42:05 +0200

    Add: css file and function that handles data

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 require(__DIR__ . "/header.php");
+require(__DIR__ . "/resources/data.php");
 ?>
 <main>
     <table>
-        <caption>Team name</caption>
+        
+        <tbody>
+            <caption>Team name</caption>
             <tr colspan="2">
             <img src="https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg" />
             </tr>
-        <tbody>
-
             <tr>
                 <th>
                     League
@@ -43,6 +44,19 @@ require(__DIR__ . "/header.php");
             </tr>
         </tbody>
     </table>
+    <?php
+        foreach($teams as $teamName => $dataName){
+            echo $teamName . "<br>";
+            echo $dataName['league'];
+            echo $dataName['uefa-coefficient-ranking'];
+            echo $dataName['city'];
+            echo $dataName['url'];
+            echo $dataName['logo'];
+            foreach($dataName['opponents'] as $opponentValue){
+                echo "<br>" . $opponentValue;
+            }
+        }
+    ?>
 </main>
 <?php
 require(__DIR__ . "/footer.php");
