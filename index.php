902ce47095e265f354e553e5657e7d91e34179e2
Your Name <laura.kotlinska.96@gmail.com>
2025-10-05 17:09:53 +0200

    Build: function creating tables and displaying data inside tables

--- index.php
+++ index.php
@@ -3,59 +3,66 @@ require(__DIR__ . "/header.php");
 require(__DIR__ . "/resources/data.php");
 ?>
 <main>
-    <table>
-        
-        <tbody>
-            <caption>Team name</caption>
-            <tr colspan="2">
-            <img src="https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg" />
-            </tr>
-            <tr>
-                <th>
-                    League
-                </th>
-                <td>
-                    data
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    Ranking
-                </th>
-                <td>
-                    data
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    City
-                </th>
-                <td>
-                    data
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    Opponents
-                </th>
-                <td>
-                    data
-                </td>  
-            </tr>
-        </tbody>
-    </table>
     <?php
-        foreach($teams as $teamName => $dataName){
-            echo $teamName . "<br>";
-            echo $dataName['league'];
-            echo $dataName['uefa-coefficient-ranking'];
-            echo $dataName['city'];
-            echo $dataName['url'];
-            echo $dataName['logo'];
-            foreach($dataName['opponents'] as $opponentValue){
-                echo "<br>" . $opponentValue;
-            }
-        }
+    foreach ($teams as $teamName => $dataName) {
+    ?>
+        <table>
+            <tbody>
+                <caption>
+                    <?php echo $teamName; ?>
+                </caption>
+                <tr colspan="2">
+                    <td>
+                        <img src="<?php echo $dataName['logo']; ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        League
+                    </th>
+                    <td>
+                        <?php echo $dataName['league']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Ranking
+                    </th>
+                    <td>
+                        <?php echo $dataName['uefa-coefficient-ranking']; ?>;
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        City
+                    </th>
+                    <td>
+                        <?php echo $dataName['city']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Opponents
+                    </th>
+                    <td>
+                        <?php
+                        foreach ($dataName['opponents'] as $opponentValue) {
+                        ?>
+                            <?php echo "<br>" . $opponentValue; ?>
+                        <?php
+                        };
+                        ?>
+                    </td>
+                </tr>
+                <tr colspan="2">
+                    <td>
+                        <button href="<?php echo $dataName['url']; ?>">Read more...</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    <?php
+    };
     ?>
 </main>
 <?php
