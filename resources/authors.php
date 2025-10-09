215d29c578a3ebfac356d78321c0d18d4635d7eb
Your Name <laura.kotlinska.96@gmail.com>
2025-10-09 11:47:38 +0200

    Add: authors.php file containing information about the authors of the website

---
+++ resources/authors.php
@@ -0,0 +1,28 @@
+<?php
+
+$authors = [
+    'Laura Kotlinska' => [
+        'hasPet' => true,
+        'petType' => 'cat',
+        'petName' => 'Taiga',
+        'likesFootball' => false,
+        'favoriteTeam' => NULL,
+        'hobby' => [
+            'gaming',
+            'series',
+        ],
+        'interest' => [
+            'frontend',
+            'programming',
+            'asian culture',
+            'food',
+        ],
+        'programmingLanguage' => [
+            'php',
+            'javascript',
+            'html/css',
+            'c#',
+            'sql'
+        ]
+    ]
+];
