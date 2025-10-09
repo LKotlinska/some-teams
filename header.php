b311126d952e89db9d7a037843df5dfeee1c64ed
Your Name <laura.kotlinska.96@gmail.com>
2025-10-09 14:38:34 +0200

    Add: heading and links to fonts

--- header.php
+++ header.php
@@ -1,20 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/png" href="/images/logo.png">
     <title>Document</title>
+    <!-- Links to google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=BBH+Sans+Bogle&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">Main</a>
-                    <a href="#">About</a>
-                </li>
-            </ul>
-        </nav>
-</header>
+        <div class="header">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Main</a>
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </nav>
+            <h1>
+                Kick Off with Your Favorite Teams
+            </h1>
+        </div>
+    </header>
