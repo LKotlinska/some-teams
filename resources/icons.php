3e9e187ef7201b9ed264785fcf8f0de4a8276679
Your Name <laura.kotlinska.96@gmail.com>
2025-10-10 14:08:28 +0200

    Add hobby icons to images and icons.php with links to icons

---
+++ resources/icons.php
@@ -0,0 +1,11 @@
+<?php
+$icons = [
+    'gaming' => '/images/icon_gaming.svg',
+    'series' => '/images/icon_movie.svg',
+    'frontend' => '/images/icon_frontend.svg',
+    'programming' => '/images/icon_programming.svg',
+    'culture' => '/images/icon_culture.svg',
+    'food' => '/images/icon_food.svg',
+    'pet' => '/images/icon_pet.svg',
+    'art' => '/images/icon_art.svg',
+];
