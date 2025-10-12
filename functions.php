f2f9d5617734cb8521aef3c2554546d6f3ce32f5
Your Name <laura.kotlinska.96@gmail.com>
2025-10-12 12:50:59 +0200

    Adds functions.php with a function to format strings

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function formatName($name){
+    // removes paranthesis and its content
+    $cleanName = preg_replace("/\s*\(.*?\)/", "", $name);
+
+    // removes backslashes
+    $cleanName = str_replace("\\", "", $cleanName);
+
+    // trims spaces and capitalize words
+    $cleanName = ucwords(strtolower(trim($cleanName)));
+
+    return $cleanName;
+}
