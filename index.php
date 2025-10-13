34d4e26477c9f5460308e999b058e7052ebc4f56
Your Name <laura.kotlinska.96@gmail.com>
2025-10-13 14:52:27 +0200

    Deletes information about the teams inside cards to display them on about teams page

--- index.php
+++ index.php
@@ -17,35 +17,8 @@ require(__DIR__ . "/resources/functions.php");
                     <?php echo $teamName; ?>
                 </h4>
                 <img class="team-logo" src="<?php echo $dataName['logo']; ?>" />
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>
-                                League
-                            </th>
-                            <td>
-                                <?php echo $cleanLeagueName; ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                Ranking
-                            </th>
-                            <td>
-                                <?php echo $dataName['uefa-coefficient-ranking']; ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                City
-                            </th>
-                            <td>
-                                <?php echo $dataName['city']; ?>
-                            </td>
-
-                    </tbody>
-                </table>
-                <a href="<?php echo $dataName['url']; ?>">Read more...</a>
+                <!-- anchor directly links to the team card on a different page -->
+                <a href="/about-teams.php#<?php echo $teamName; ?>">Read more</a>
             </div>
         <?php
         };
